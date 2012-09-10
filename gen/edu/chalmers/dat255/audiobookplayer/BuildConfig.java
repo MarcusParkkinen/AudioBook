1aad0816718514709fb40829c6b0fa5a74b64444
Marcus Parkkinen <epinefrema@gmail.com>
2012-09-10 11:40:21 +0200

    Startup

---
+++ gen/edu/chalmers/dat255/audiobookplayer/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.chalmers.dat255.audiobookplayer;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
