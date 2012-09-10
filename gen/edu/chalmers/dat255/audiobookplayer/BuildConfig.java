f358a5101bd65c5d1adb8f7fd27a0308fe409c0c
Marcus Parkkinen <epinefrema@gmail.com>
2012-09-10 11:50:04 +0200

    cleaned up binary files from the repository

--- gen/edu/chalmers/dat255/audiobookplayer/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package edu.chalmers.dat255.audiobookplayer;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
