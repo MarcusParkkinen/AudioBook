30488b0ff8182997fa4e381fc5d6d5dfe3d6c733
MarcusParkkinen <marpar@student.chalmers.se>
2012-09-19 16:26:40 +0200

    Renamed controller-package.

---
+++ src/edu/chalmers/dat255/audiobookplayer/ctrl/PlayerController.java
@@ -0,0 +1,27 @@
+package edu.chalmers.dat255.audiobookplayer.ctrl;
+
+import java.util.List;
+
+public class PlayerController {
+	
+	public void changeOrder(int index1, int index2) {
+		
+	}
+	
+	public void pause() {
+		
+	}
+	
+	public void play() {
+		
+	}
+	
+	public void seek(long time) {
+		
+	}
+	
+	public void setTrackList(List<String> trackList) {
+		
+	}
+	
+}
