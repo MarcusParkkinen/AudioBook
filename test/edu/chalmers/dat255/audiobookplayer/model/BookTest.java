4f00656f30ad81a97d5d19db8ff3294c085984a3
akik <akikakela@live.se>
2012-09-28 19:30:10 +0200

    Added generated test classes.

---
+++ test/edu/chalmers/dat255/audiobookplayer/model/BookTest.java
@@ -0,0 +1,11 @@
+package edu.chalmers.dat255.audiobookplayer.model;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Aki Käkelä
+ *
+ */
+public class BookTest extends TestCase {
+
+}
---
+++ test/edu/chalmers/dat255/audiobookplayer/model/TrackTest.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package edu.chalmers.dat255.audiobookplayer.model;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Aki Käkelä
+ *
+ */
+public class TrackTest extends TestCase {
+
+	/**
+	 * @param name
+	 */
+	public TrackTest(String name) {
+		super(name);
+	}
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}
